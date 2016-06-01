bfa8a41519eabe03134d50f0ab1f8a0e4b9acc6c
Lars Hinrichs <wordsmith189@users.noreply.github.com>
2016-06-01 10:53:09 -0500

    Update mixed logistic.R

--- mixed logistic.R
+++ mixed logistic.R
@@ -1,8 +1,11 @@
+# Load packages with functions for model fitting and utilities
+library(lme4)
 library(mlmRev)
-m1<-lmer(use~age + (1|district), Contraception, family=binomial)
+m1 <- lmer(use ~ age + (1|district), Contraception, family=binomial)
 fitted <- fitted(m1)
 predicted <- ifelse(fitted  >= .5, 1,0)
 a <- data.frame(Contraception, predicted)
 
+# Make a crosstab of observed by predicted outcomes, including statistics
 library(gmodels)
 CrossTable(a$use, a$predicted)
